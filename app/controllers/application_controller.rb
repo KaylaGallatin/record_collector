2d4923615e4f67594e5acaab139cc1d09bc58ee3
KaylaGallatin <kaylagal@verizon.net>
2019-08-25 14:28:24 -0500

    Set up layouts and change up the colors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   before_action :authenticate_user!
+
+  private
+
+  def layout_by_resource
+    if devise_controller? || controller_path == "home"
+      "home"
+    else
+      "application"
+    end
+  end
 end
