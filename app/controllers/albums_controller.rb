f20ed7f3bcfba87980e6b55bdce8eadd47d0d6ec
KaylaGallatin <kaylagal@verizon.net>
2019-09-07 12:54:02 -0500

    Add routes, model, and controller for albums

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,56 @@
+class AlbumsController < ApplicationController
+  before_action :set_album, only: %i(show edit update destroy)
+
+  def index
+    @albums = Album.where(user_id: current_user.id)
+  end
+
+  def show
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new
+    @album.assign_attributes(album_params)
+    if @album.valid? && @album.save
+      redirect_to album_path(@album),
+        notice: 'Record successfully created'
+    else
+      render :new, alert: 'Record could not be saved'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @album.assign_attributes(album_params)
+    if @album.valid? && @album.save
+      redirect_to album_path(@album),
+        notice: 'Record updated successfully'
+    else
+      render :new, alert: 'Record could not be updated'
+    end
+  end
+
+  def destroy
+    @album.destroy
+    redirect_to albums_path, notice: 'Record was successfully deleted.'
+  end
+
+  private
+
+    def set_album
+      @album = Album.find(params[:id])
+    end
+
+    def album_params
+      params.require(:album)
+            .permit(:title, :release_date, :description)
+            .merge(:user_id: current_user.id)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For a list of available routes run "rake routes" in terminal
 Rails.application.routes.draw do
+  resources :albums
   devise_for :users
   resources :users, only: [:show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 end
